e6fc73392b3e547c520fa2cf8ef951016e64f5b0
Juan Carlos <juank20097@gmail.com>
2022-11-09 22:38:44 -0500

    Correction Many to One Select Departments

--- sicpaFront/src/app/components/Department/departmentadd/departmentadd.component.ts
+++ sicpaFront/src/app/components/Department/departmentadd/departmentadd.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Department } from 'src/app/models/department';
+import { Enterprise } from 'src/app/models/enterprise';
 import { DepartmentService } from 'src/app/services/serviceDepartment/department.service';
 
 @Component({
@@ -11,9 +12,13 @@ import { DepartmentService } from 'src/app/services/serviceDepartment/department
 export class DepartmentaddComponent implements OnInit {
 
   department=new Department();
+  enterprises!:Enterprise[];
   constructor(private router:Router, private service:DepartmentService) { }
 
   ngOnInit(): void {
+    this.service.getEnterprise().subscribe(data=>{
+      this.enterprises=data;
+    })
   }
 
   Save(){
--- sicpaFront/src/app/services/serviceDepartment/department.service.ts
+++ sicpaFront/src/app/services/serviceDepartment/department.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Department } from 'src/app/models/department';
+import { Enterprise } from 'src/app/models/enterprise';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,18 @@ export class DepartmentService {
   constructor(private http:HttpClient) { }
 
   Url= 'http://localhost:8080/department';
+  Url2= 'http://localhost:8080/enterprise';
 
   getDepartment(){
     return this.http.get<Department[]>(this.Url);
+  }
+
+  getEnterprise(){
+    return this.http.get<Enterprise[]>(this.Url2);
+  }
+
+  getEnterpriseId(id:number){
+    return this.http.get<Enterprise>(this.Url2+"/"+id);
   }
 
   createDepartment(department:Department){
